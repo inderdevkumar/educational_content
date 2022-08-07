a0d7e72259d0f474ac776d2831f73954db655cd8
inderdevkumar <id0102yadav@gmail.com>
2022-08-07 18:19:07 +0200

    $So 7. Aug 18:19:07 CEST 2022

---
+++ jenkins_project/ansible_to_run_jenkins/bash_for_ansible/create_file_bash_ansible_with_parameter.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+ansible-playbook /home/inder/my_data/educational_content/jenkins_project/ansible_to_run_jenkins/playbook_for_jenkins/$ansible_playbook.yml -i /home/inder/my_data/educational_content/jenkins_project/ansible_to_run_jenkins/inventory_for_jenkins/inventory4.yml
