70f09eabbc254aeee54ead719b18075dec7ed585
inderdevkumar <id0102yadav@gmail.com>
2022-08-09 21:30:58 +0200

    $Di 9. Aug 21:30:57 CEST 2022

--- jenkins_project/ansible_to_run_jenkins/bash_for_ansible/create_file_with_host_parameter_in_vm_ansible.sh
+++ jenkins_project/ansible_to_run_jenkins/bash_for_ansible/create_file_with_host_parameter_in_vm_ansible.sh
@@ -6,20 +6,13 @@ echo ================
 
 home_path="/home/inder/my_data/educational_content/jenkins_project/ansible_to_run_jenkins"
 
-echo hostn_name
-#read host_name
+echo i am running with user
+whoami
+echo Running with sudo
 
-echo playbook
-#read ansible_playbook
-echo hostname is : $host_name
-echo playbook is: $ansible_playbook.yml
-sudo -S mkdir /tmp/for_sed
+sudo -S sed -i -e "s/hostname1/$host_name/g" $home_path/playbook_for_jenkins/$ansible_playbook.yml
 kumar0102
-sudo sed -i -e "s/hostname1/$host_name/g" $home_path/playbook_for_jenkins/$ansible_playbook.yml
 
-#> /tmp/create_file_playbook
-
-#mv /tmp/create_file_playbook $home_path/playbook_for_jenkins/$ansible_playbook.yml
 
 ansible-playbook $home_path/playbook_for_jenkins/$ansible_playbook.yml -i $home_path/inventory_for_jenkins/inventory4.yml
 
