d9ff7564a1804fc2e35a6d46d972f637470330f5
inderdevkumar <id0102yadav@gmail.com>
2022-08-09 22:24:39 +0200

    $Di 9. Aug 22:24:39 CEST 2022

--- jenkins_project/ansible_to_run_jenkins/bash_for_ansible/create_file_with_host_parameter_in_vm_ansible.sh
+++ jenkins_project/ansible_to_run_jenkins/bash_for_ansible/create_file_with_host_parameter_in_vm_ansible.sh
@@ -12,6 +12,9 @@ echo Running with sudo
 
 #sudo sed -i -e "s/hostname1/$host_name/g" $home_path/playbook_for_jenkins/$ansible_playbook.yml
 sudo sed -i -e "s/hostname1/inder1/g" $home_path/playbook_for_jenkins/$ansible_playbook.yml
+echo create files
+sudo touch $home_path/playbook_for_jenkins/${host_name}_with_Sudo.txt
+touch $home_path/playbook_for_jenkins/${host_name}_without_sudo.txt
 #kumar0102
 
 
