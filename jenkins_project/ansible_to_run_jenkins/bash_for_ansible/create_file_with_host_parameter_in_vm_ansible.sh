2b6a836b8a0d1be30fde12114ef15b6af56ed76b
inderdevkumar <id0102yadav@gmail.com>
2022-08-10 17:31:40 +0200

    $Mi 10. Aug 17:31:40 CEST 2022

--- jenkins_project/ansible_to_run_jenkins/bash_for_ansible/create_file_with_host_parameter_in_vm_ansible.sh
+++ jenkins_project/ansible_to_run_jenkins/bash_for_ansible/create_file_with_host_parameter_in_vm_ansible.sh
@@ -4,6 +4,8 @@ echo =================
 echo starting
 echo ================
 
+export ANSIBLE_HOST_KEY_CHECKING=False
+
 home_path="/home/inder/my_data/educational_content/jenkins_project/ansible_to_run_jenkins"
 
 echo i am running with user
@@ -11,14 +13,18 @@ whoami
 echo Running with sudo
 
 #sudo sed -i -e "s/hostname1/$host_name/g" $home_path/playbook_for_jenkins/$ansible_playbook.yml
-sudo sed -i -e "s/hostname1/inder1/g" $home_path/playbook_for_jenkins/$ansible_playbook.yml
-echo create files
-sudo touch $home_path/playbook_for_jenkins/${host_name}_with_Sudo.txt
-touch $home_path/playbook_for_jenkins/${host_name}_without_sudo.txt
+echo 'kumar0102' | sudo -S sed -i -e "s/hostname1/$host_name/g" $home_path/playbook_for_jenkins/$ansible_playbook.yml
+#echo create files
+#sudo touch $home_path/playbook_for_jenkins/${host_name}_with_Sudo.txt
+#echo 'kumar0102' | sudo -S touch $home_path/playbook_for_jenkins/${host_name}_with_Sudo2.txt
+
+#touch $home_path/playbook_for_jenkins/${host_name}_without_sudo.txt
 #kumar0102
 
 
 ansible-playbook $home_path/playbook_for_jenkins/$ansible_playbook.yml -i $home_path/inventory_for_jenkins/inventory4.yml
+
+echo 'kumar0102' | sudo -S sed -i -e "s/$host_name/hostname1/g" $home_path/playbook_for_jenkins/$ansible_playbook.yml
 
 echo =================
 echo The end
