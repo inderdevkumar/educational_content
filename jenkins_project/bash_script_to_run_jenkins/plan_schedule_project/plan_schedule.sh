993010d96f18a8bb2098c721f4c88c033f1f26ae
inderdevkumar <id0102yadav@gmail.com>
2022-08-13 19:48:38 +0200

    $Sa 13. Aug 19:48:38 CEST 2022

--- jenkins_project/bash_script_to_run_jenkins/plan_schedule_project/plan_schedule.sh
+++ jenkins_project/bash_script_to_run_jenkins/plan_schedule_project/plan_schedule.sh
@@ -7,8 +7,8 @@ echo "Say Yes if train is booked else No"
 read del_book
 
 if [[  ( $del_book  == "Yes")  ]]; then
-echo "Please Enter the Train Seat Number : "
-read seat_num1
+echo "Please Enter the Train Boggy with Seat Number : "
+read del_kqr_seat_num
 else
 echo "Train is not booked yet..."
 fi     
@@ -19,8 +19,8 @@ echo "Say Yes if train is booked else No"
 read bbs_kqr_book
 
 if [[  ( $bbs_kqr_book  == "Yes")  ]]; then
-echo "Please Enter the Train Seat Number : "
-read seat_num2
+echo "Please Enter the Train Boggy with Seat Number : "
+read bbs_kqr_seat_num
 else
 echo "Train is not booked yet..."
 fi
@@ -32,8 +32,8 @@ echo "Say Yes if train is booked else No"
 read kqr_bbs_book
 
 if [[  ( $kqr_bbs_book  == "Yes")  ]]; then
-echo "Please Enter the Train Seat Number : "
-read seat_num3
+echo "Please Enter the Train Boggy with Seat Number : "
+read kqr_bbs_seat_num
 else
 echo "Train is not booked yet..."
 fi
@@ -46,22 +46,47 @@ echo "Say Yes if train is booked else No"
 read kqr_puri_book
 
 if [[  ( $kqr_puri_book  == "Yes")  ]]; then
-echo "Please Enter the Train Seat Number : "
-read seat_num4
+echo "Please Enter the Train Boggy with  Seat Number : "
+read kqr_puri_seat_num
 else
 echo "Train is not booked yet..."
 fi
 
 
-
-echo "***  4.  Journey from KQR to Puri on 9th Dec *** "
+echo "***  5.  Journey from Puri to NJP on 10th Dec *** "
 
 echo "Say Yes if train is booked else No"
-read kqr_puri_book
+read puri_njp_book
 
-if [[  ( $kqr_puri_book  == "Yes")  ]]; then
-echo "Please Enter the Train Seat Number : "
-read seat_num4
+if [[  ( $puri_njp_book  == "Yes")  ]]; then
+echo "Please Enter the Train Boggy with  Seat Number : "
+read puri_njp_seat_num
+else
+echo "Train is not booked yet..."
+fi
+
+
+echo "***  6.  Journey from NJP to Howrah on 17th Dec *** "
+
+echo "Say Yes if train is booked else No"
+read njp_hwrah_book
+
+if [[  ( $njp_hwrah_book  == "Yes")  ]]; then
+echo "Please Enter the Train Boggy with  Seat Number : "
+read njp_howrh_seat_num
+else
+echo "Train is not booked yet..."
+fi
+
+
+echo "***  7.  Journey from  Howrah to hyderabad on 18th Dec *** "
+
+echo "Say Yes if train is booked else No"
+read hwr_hyd_book
+
+if [[  ( $hwr_hyd_book  == "Yes")  ]]; then
+echo "Please Enter the Train boggy with  Seat Number : "
+read hwr_hyd_seat_num
 else
 echo "Train is not booked yet..."
 fi
