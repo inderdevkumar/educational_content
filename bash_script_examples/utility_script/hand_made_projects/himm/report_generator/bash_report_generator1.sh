e24300bb54e81ecb3169ba6777b812e4cd9ac8f9
inderdevkumar <id0102yadav@gmail.com>
2022-08-30 22:25:26 +0200

    $Di 30. Aug 22:25:26 CEST 2022

--- bash_script_examples/utility_script/hand_made_projects/himm/report_generator/bash_report_generator1.sh
+++ bash_script_examples/utility_script/hand_made_projects/himm/report_generator/bash_report_generator1.sh
@@ -31,4 +31,6 @@ echo test csv to key value pair
 #awk -F, 'NR==1{for(i=1;i<=NF;i++)Arr[i]=$i;next}{printf("%s ",$1);for(j=2;j<=NF-3;j++){printf("%s: %s  ",Arr[j],$j)}printf("\n")}' /home/inder/my_data/doc/hit_count.csv
 
 
-awk -F, 'NR==1{for(i=1;i<=NF;i++)Arr[i]=$i;next}{for(j=1;j<=NF;j++){printf("%s: %s  ",Arr[j],$j)}printf("\n")}' /home/inder/my_data/doc/hit_count.csv > /home/inder/my_data/doc/all_filtered_output.txt
+awk -F, 'NR==1{for(i=1;i<=NF;i++)Arr[i]=$i;next}{for(j=1;j<=NF;j++){printf("%s: %s  ",Arr[j],$j)}printf("\n")}' /home/inder/my_data/doc/hit_count.csv  > /home/inder/my_data/doc/all_filtered_output.txt
+
+awk -F, 'NR==1{for(i=1;i<=NF;i++)Arr[i]=$i;next}{for(j=1;j<=NF;j++){printf("%s: %s  ",Arr[j],$j)}printf("\n")}' /home/inder/my_data/doc/no_hit_count.csv  >> /home/inder/my_data/doc/all_filtered_output.txt
