d0900b834ad3caf6de885e783b4674fd019c63a4
inderdevkumar <id0102yadav@gmail.com>
2022-09-02 18:26:34 +0200

    $Fr 2. Sep 18:26:33 CEST 2022

--- bash_script_examples/utility_script/hand_made_projects/to_find_any_folder/to_find_any_folder.sh
+++ bash_script_examples/utility_script/hand_made_projects/to_find_any_folder/to_find_any_folder.sh
@@ -6,14 +6,20 @@
 
 echo "Enter 1 for searching a folder and 2 for searching a file"
 read search_num
+
 if [ $search_num == 1 ]; then
 	echo "Enter the directory name you want to check: "
         read dir_name
-        sudo find / -type d -name $dir_name
+        #find / -type d -name $dir_name  #To find dir in all system
+	find /home/inder/my_data/ -type d  -name $dir_name    #TO find dir in particular dir and subdir
+
 elif [ $search_num == 2 ]; then
 	echo "Enter the file name you want to check: "
         read file_name
-	sudo find / -type f -name $file_name
+	#find / -type f -name $file_name
+	find /home/inder/my_data/ -type f -name $file_name
+
 else
 	echo "Please enter either 1 or 2"
+
 fi
