dc4ed1392d36156df56693b8e8fd8b6969c9c12f
inderdevkumar <id0102yadav@gmail.com>
2022-10-08 14:06:53 +0200

    $Sa 8. Okt 14:06:53 CEST 2022

--- bash_script_examples/send_email_date/date_parse.sh
+++ bash_script_examples/send_email_date/date_parse.sh
@@ -5,6 +5,18 @@ Day=`date +%d`
 Hour=`date +%H`
 Minute=`date +%M`
 Second=`date +%S`
+
 echo `date`
 echo "Current Date is: $Day-$Month-$Year"
 echo "Current Time is: $Hour:$Minute:$Second"
+
+echo waiting for 10s
+sleep 10
+Hour2=`date +%H`
+Minute2=`date +%M`
+Second2=`date +%S`
+echo "Current Time is: $Hour2:$Minute2:$Second2"
+echo "Current Time is: $Hour:$Minute:$Second"
+Hour_dff= 1
+
+echo Time Difference is: 
