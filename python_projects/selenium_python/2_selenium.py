4f243b6da9bc36ab786ce9d852ec4e8a64122a65
inderdevkumar <id0102yadav@gmail.com>
2023-02-14 15:27:16 +0100

    $Di 14. Feb 15:27:15 CET 2023

---
+++ python_projects/selenium_python/2_selenium.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+
+browser= webdriver.Firefox(executable_path= "/home/inder/my_data/doc/webdriver/geckodriver-v0.32.2-linux32/geckodriver")
+
+
+browser.get("https://phoenixnap.com/kb/extract-tar-gz-files-linux-command-line")
+
+#browser.close()
+
