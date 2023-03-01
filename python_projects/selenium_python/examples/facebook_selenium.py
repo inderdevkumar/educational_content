d24b7be3d7fd3aa7c5a0326ba33ba99bc21753a3
inderdevkumar <id0102yadav@gmail.com>
2023-03-01 15:17:42 +0100

    $Mi 1. Mär 15:17:42 CET 2023

--- python_projects/selenium_python/examples/facebook_selenium.py
+++ python_projects/selenium_python/examples/facebook_selenium.py
@@ -3,6 +3,7 @@ from webdriver_manager.firefox import GeckoDriverManager
 import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
 
 class class_find_element_by_xpath:
    
@@ -21,17 +22,28 @@ class class_find_element_by_xpath:
 
         time.sleep(3)
         # CLick friends
-        click_friends= driver.find_element(By.XPATH, "//input[@type='search']")
-        click_friends.send_keys("Himadri Tanaya")
-        click_friends.send_keys(Keys.ENTER)
-        time.sleep(8)
+        list_of_friends= ["Himadri Tanaya", "Anubhav Kumar", "Anand Kumar Aky"]
+
+        for item in list_of_friends:
+
+            click_friends= driver.find_element(By.XPATH, "//input[@type='search']")
+            click_friends.send_keys(item)
+            click_friends.send_keys(Keys.ENTER)
+            time.sleep(10)
 
         # Click on message
-        message_btn= driver.find_element(By.XPATH, "//span[contains(text(),'Message')]")
-        message_btn.click()
-        time.sleep(3)
-        message_btn.send_keys("Hi")
-        message_btn.send_keys(Keys.ENTER)
+            message_btn= driver.find_element(By.XPATH, "//span[contains(text(),'Message')]")
+            message_btn.click()
+            time.sleep(3)
+        
+            content= f"Hi {item.split()[0]}, You are getting this message from automated script. Test is going on. Plz dont reply back, as I am not online:). Thanks!"
+        # Write some message
+            actions = ActionChains(driver)
+            actions.send_keys(content)
+            actions.send_keys(Keys.ENTER)
+            actions.perform()
+            time.sleep(3)
+
         driver.close()
 
 cls_obj= class_find_element_by_xpath()
